73ec51a8e0a823d63feaec76fd2dc3dd076e5cbd
Sherman Au <shermau13@gmail.com>
2022-05-23 17:20:17 -0600

    login form and password auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  def new
-
-  end
+  def new; end
 
   def show
     @user = User.find(params[:id])
@@ -9,12 +7,31 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-    redirect_to "/users/#{user.id}"
+    if user.save
+      redirect_to "/users/#{user.id}"
+    else
+      flash.now[:alert] = user.errors.full_messages.to_sentence.to_s
+      render 'new'
+    end
+  end
+
+  def login_form; end
+
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome #{user.name}"
+      redirect_to "/users/#{user.id}"
+    else
+      flash[:error] = 'Invalid email/password'
+      render :login_form
+    end
   end
 
   private
 
   def user_params
-      params.permit(:name, :email)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
